0916372c17983e03909ee74370a19101ea63d48f
MemphisCornelius <39841089+MemphisCornelius@users.noreply.github.com>
2018-07-18 20:10:58 +0200

    1.17.2
    +added forgotten pom.xml
    +added in DVCbG that uper and lowecase are ignored

--- src/main/java/listeners/VoicechatListener.java
+++ src/main/java/listeners/VoicechatListener.java
@@ -20,7 +20,7 @@ public class VoicechatListener extends ListenerAdapter {
         for (Member m : list1) {
             for (Member m1 : list2) {
 
-                if (!m.getGame().getName().equals(m1.getGame().getName())) {
+                if (!m.getGame().getName().toLowerCase().equals(m1.getGame().getName().toUpperCase())) {
                     return false;
                 }
             }
