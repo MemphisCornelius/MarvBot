f7c7d0a9d0d7d5af10ba42129067f0038544af5e
MemphisCornelius <theeasyminer@gmail.com>
2020-05-31 15:08:17 +0200

    cleanup

--- src/main/java/commands/CmdVersion.java
+++ src/main/java/commands/CmdVersion.java
@@ -5,7 +5,6 @@ import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import util.Config;
-import util.GitHubConnector;
 import util.MessageMask;
 import util.Time;
 
